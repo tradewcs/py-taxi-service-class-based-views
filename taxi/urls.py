a433846da37fea6bd9ea21b390e437a5700d80b5
Mike <tradewcs@gmail.com>
2025-12-11 14:46:03 +0200

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,30 @@
 from django.urls import path
 
 from .views import index
+from . import views
 
 urlpatterns = [
     path("", index, name="index"),
+
+    path(
+        "manufacturers/",
+        views.ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
+
+    path("cars/", views.CarListView.as_view(), name="car-list"),
+    path(
+        "car/<int:pk>/",
+        views.CarDetailView.as_view(),
+        name="car-detail"
+    ),
+
+    path("drivers/", views.DriverListView.as_view(), name="driver-list"),
+    path(
+        "driver/<int:pk>/",
+        views.DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,38 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+    template_name = "taxi/manufacturer_list.html"
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.all().select_related("manufacturer")
+    paginate_by = 5
+    template_name = "taxi/car_list.html"
+    context_object_name = "car_list"
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+    context_object_name = "car"
+
+
+class DriverListView(ListView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer").all()
+    paginate_by = 5
+    template_name = "taxi/driver_list.html"
+    context_object_name = "driver_list"
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    context_object_name = "driver"
